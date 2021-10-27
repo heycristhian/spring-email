b84d37ae1fc476bc7241d984e087d432b172bb4e
heycristhian <heycristhian@gmail.com>
2021-10-27 00:38:12 -0300

    First commit

---
+++ src/test/java/com/heycristhian/springemail/SpringEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heycristhian.springemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
